6b3e392ffe8898ede6fbabdea74f685c4f5335a4
Enrique Moreno <jemc300x@gmail.com>
2024-11-30 20:20:10 -0500

    Add paginator

--- src/app/pages/customers/customers.component.ts
+++ src/app/pages/customers/customers.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {
   TableColumn,
+  TableConfig,
   UiTableComponent,
 } from '../../shared/components/ui-table/ui-table.component';
 import { timer } from 'rxjs';
+import { PageEvent } from '@angular/material/paginator';
 
 interface Customer {
   name: string;
@@ -23,10 +25,26 @@ export class CustomersComponent implements OnInit {
   customers: Customer[] = [];
   tableColumns: TableColumn<Customer>[] = [];
   isLoadingCustomers = true;
+  tableConfig!: TableConfig;
 
   ngOnInit(): void {
+    this.setTableConfig();
     this.getCustomers();
     this.setTableColumns();
+  }
+
+  onPageEventTable(event: PageEvent) {
+    console.log('PageEvent: ', event);
+  }
+
+  setTableConfig() {
+    this.tableConfig = {
+      paginator: {
+        show: true,
+        showFirstLastButtons: true,
+        // totalRecords: 0,
+      },
+    };
   }
 
   setTableColumns() {
@@ -56,6 +74,7 @@ export class CustomersComponent implements OnInit {
 
   getCustomers() {
     timer(2000).subscribe(() => {
+      this.tableConfig.paginator.totalRecords = 300000;
       this.isLoadingCustomers = false;
       this.customers = [
         {
@@ -84,6 +103,36 @@ export class CustomersComponent implements OnInit {
         },
         {
           name: 'Luisto',
+          lastname: 'Comunica',
+          dob: new Date(1985, 5, 1),
+          gender: 'm',
+        },
+        {
+          name: 'Hola2',
+          lastname: 'Mundo',
+          dob: new Date(1980, 0, 1),
+          gender: 'm',
+        },
+        {
+          name: 'Juan2',
+          lastname: 'Perez',
+          dob: new Date(1981, 1, 1),
+          gender: 'm',
+        },
+        {
+          name: 'Juana2',
+          lastname: 'Perca',
+          dob: new Date(1982, 1, 1),
+          gender: 'f',
+        },
+        {
+          name: 'Ana2',
+          lastname: 'Pacco',
+          dob: new Date(1984, 4, 1),
+          gender: 'f',
+        },
+        {
+          name: 'Luisto2',
           lastname: 'Comunica',
           dob: new Date(1985, 5, 1),
           gender: 'm',
--- src/app/shared/components/ui-table/ui-table.component.ts
+++ src/app/shared/components/ui-table/ui-table.component.ts
@@ -1,10 +1,18 @@
 import {
+  AfterViewInit,
   Component,
   computed,
   input,
   OnChanges,
+  output,
   SimpleChanges,
+  viewChild,
 } from '@angular/core';
+import {
+  MatPaginator,
+  MatPaginatorModule,
+  PageEvent,
+} from '@angular/material/paginator';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 export interface TableColumn<T> {
@@ -13,24 +21,45 @@ export interface TableColumn<T> {
   content: (row: T) => string | null | undefined;
 }
 
+export interface TableConfig {
+  paginator: {
+    show?: boolean;
+    pageSizeOptions?: number[];
+    showFirstLastButtons?: boolean;
+    totalRecords?: number;
+  };
+}
+
 @Component({
   selector: 'ui-table',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatPaginatorModule],
   templateUrl: './ui-table.component.html',
   styleUrl: './ui-table.component.scss',
 })
-export class UiTableComponent<T> implements OnChanges {
+export class UiTableComponent<T> implements OnChanges, AfterViewInit {
   dataSource = new MatTableDataSource<T>([]);
   data = input<T[]>([]);
   columns = input<TableColumn<T>[]>([]);
   displayedColumns = computed(() => this.columns().map((col) => col.def));
   isLoading = input(false);
+  config = input<TableConfig>();
+  pageEvent = output<PageEvent>();
+
+  private readonly paginator = viewChild(MatPaginator);
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['data'].currentValue) {
       this.setData();
     }
+  }
+
+  ngAfterViewInit(): void {
+    if (this.config()?.paginator.totalRecords !== undefined) {
+      return;
+    }
+
+    this.dataSource.paginator = this.paginator() ?? null;
   }
 
   private setData() {
