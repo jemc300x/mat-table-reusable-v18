66e216ae1bf9d8a592deeb1cdb059a71451ddc24
Enrique Moreno <emorenocode@gmail.com>
2024-12-20 21:43:42 -0500

    Revert "Add MatSidenav"
    This reverts commit 5f7b66891a9432891a0432655443c00ced4d54a5.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,14 @@
 import { Routes } from '@angular/router';
 import { CustomersComponent } from './pages/customers/customers.component';
 import { ProductsComponent } from './pages/products/products.component';
-import { DashboardComponent } from './layout/dashboard/dashboard.component';
 
 export const routes: Routes = [
   {
-    path: '',
-    component: DashboardComponent,
-    children: [
-      {
-        path: 'customers',
-        component: CustomersComponent,
-      },
-      {
-        path: 'products',
-        component: ProductsComponent,
-      },
-    ],
+    path: 'customers',
+    component: CustomersComponent,
+  },
+  {
+    path: 'products',
+    component: ProductsComponent,
   },
 ];
