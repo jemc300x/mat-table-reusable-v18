5f7b66891a9432891a0432655443c00ced4d54a5
Enrique Moreno <emorenocode@gmail.com>
2024-12-20 21:39:38 -0500

    Add MatSidenav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,21 @@
 import { Routes } from '@angular/router';
 import { CustomersComponent } from './pages/customers/customers.component';
 import { ProductsComponent } from './pages/products/products.component';
+import { DashboardComponent } from './layout/dashboard/dashboard.component';
 
 export const routes: Routes = [
   {
-    path: 'customers',
-    component: CustomersComponent,
-  },
-  {
-    path: 'products',
-    component: ProductsComponent,
+    path: '',
+    component: DashboardComponent,
+    children: [
+      {
+        path: 'customers',
+        component: CustomersComponent,
+      },
+      {
+        path: 'products',
+        component: ProductsComponent,
+      },
+    ],
   },
 ];
